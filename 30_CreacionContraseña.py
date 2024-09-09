e87d45ce0e0d5b18a95eb8c51f7ec7afc2aa6bd9
SebasGS <Sebasguesa@hotmail.com>
2024-09-09 07:17:23 -0500

    Creador de contraseñas aleatorias

---
+++ 30_CreacionContraseña.py
@@ -0,0 +1,24 @@
+﻿#pip install random
+import random
+
+#En la siguiente linea se ingresa la cantidad de caracteres que va a tener la contraseña
+cantidad_caracteres = int(input("Porfavor ingrese la cantidad de caracteres: "))
+
+#Se declaran los posibles caracteres que va a tener la contraseña
+minuscula = "abcdefghijklmnñopqrstuvwxyz"
+mayuscula = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
+numeros = "0123456789"
+simbolos = "!@#$%&/()=?¡¨*][_:;><]"
+
+#Se agrupan todos los carecteres posibles en una variable
+todos_los_caracteres = minuscula + mayuscula + numeros + simbolos
+
+#Se crea la contraseña con la posiblidad de obtener todos los caracteres y se le adjunta la cantidad
+contraseña = ''.join(random.sample(todos_los_caracteres, cantidad_caracteres))
+
+print("La contraseña creada es : " + contraseña)
+
+ 
+
+
+
