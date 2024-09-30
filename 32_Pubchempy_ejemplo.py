d39f48e4e6d0793ab342426f8aec2edab15f45d0
SebasGS <Sebasguesa@hotmail.com>
2024-09-30 09:45:53 -0500

    Ejemplo del uso de la libreria Pubchempy editado

--- 32_Pubchempy_ejemplo.py
+++ 32_Pubchempy_ejemplo.py
@@ -3,16 +3,17 @@ import pubchempy
 import pubchempy as pcp
 
 #Ingrese el nombre de el quimico en ingles
-chemical_name = input("Ingrese el nombre del elemento quimico en ingles: ")
+nombre_quimico_en = input("Ingrese el nombre del elemento quimico en ingles: ")
 
 #Se trae la informacion relacionada a ese nombre de ese quimico 
-compound = pcp.get_compounds(chemical_name, 'name')[0]
+componentes_quimicos = pcp.get_compounds(nombre_quimico_en, 'name')[0]
 
 #Con el siguiente codigo se imprime en pantalla la informacon
-print(f"Nombre IUPAC: {compound.iupac_name}")
-print(f"Nombre comun: {compound.synonyms[0]}")
-print(f"El peso molecular: {compound.molecular_weight}")
-print(f"Formula: {compound.molecular_formula}")
+print(f"Nombre IUPAC: {componentes_quimicos.iupac_name}")
+print(f"Simbolo quimico: {componentes_quimicos.molecular_formula}")
+print(f"Nombre comun: {componentes_quimicos.synonyms[0]}")
+print(f"El peso molecular: {componentes_quimicos.molecular_weight}")
+
 
 
     
