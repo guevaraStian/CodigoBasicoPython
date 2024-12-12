595578a0d40d43a8c2fd1b01e33d21addb4dd682
SebasGS <Sebasguesa@hotmail.com>
2024-12-12 16:16:25 -0500

    Uso de libreria turtle para crear una flor

---
+++ 45_Turtle_ejemplo_flor.py
@@ -0,0 +1,32 @@
+# Primero se instala turtle con el siguiente comando "pip install turtle"
+# Luego de bajar librerias, las importamos
+import turtle
+
+# Se crean las variables que muestran el dibujo de la flor
+t= turtle.Turtle()
+s= turtle.Screen()
+s.bgcolor('#474747')
+t.pencolor('#ff0000')
+t.speed(100)
+
+# Creamos la variable que tiene los colores que se usaran en la sombraa de la flor
+col=('#ff5000','#ff5500','#ff5555','#ff8888')
+
+# El siguiente grupo de for es la descripcion de el camino que hara el objeto dibujante
+# Recorriendo un cuadro de diferentes colores
+for n in range(5):
+    for x in range(8):
+        t.speed(x+10)
+        for i in range(2):
+            t.pensize(2)
+            t.circle(90+n*20,90)
+            t.lt(90)
+        t.lt(45)
+    t.pencolor(col[n%4])
+s.exitonclick()
+
+
+
+
+
+
