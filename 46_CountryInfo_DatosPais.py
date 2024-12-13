c30afcc9e56309d0845e3421d52c1154675b9bee
SebasGS <Sebasguesa@hotmail.com>
2024-12-13 09:17:37 -0500

    Informacion de un pais con la libreria countryinfo

---
+++ 46_CountryInfo_DatosPais.py
@@ -0,0 +1,16 @@
+# Primero se instala countryinfo con el siguiente comando "pip install countryinfo"
+# Luego de bajar librerias, las importamos
+from countryinfo import CountryInfo
+
+# Se guarda en una variable el nombre de un pais en el idioma ingles
+Nombre_Pais = input("Ingrese el nombre de un pais en el idioma ingles: ")
+
+# luego guardamos la informacion en una variable, despues de consultar el nombre del pais
+info_pais = CountryInfo(Nombre_Pais)
+
+# El siguiente codigos se imprime en pantalla diferentes datos del pais consultado
+print("La capital es :", info_pais.capital())
+print("Simbolos de su moneda : ", info_pais.currencies())
+print("El idioma mas usado es : ", info_pais.languages())
+print("Los paises en frontera son : ", info_pais.borders())
+print("Nombre completo : ", info_pais.alt_spellings())
